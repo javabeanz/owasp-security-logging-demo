ff721b2d8fafb5835afb5bcac81af1822a1e2412
javabeanz <java.development@gmail.com>
2016-07-23 11:57:33 +0200

    first commit

---
+++ src/test/java/org/owasp/security/logging/DemoApplicationTests.java
@@ -0,0 +1,19 @@
+package org.owasp.security.logging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.owasp.security.logging.demo.DemoApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DemoApplication.class)
+@WebAppConfiguration
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
